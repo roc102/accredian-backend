2e47f871677bf06847d8afe59cd69ea4fb390d72
roc102 <kyroc102@gmail.com>
2023-12-18 07:17:42 +0530

    update

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/signin',(req,res) => {
             db.select('*').from('users')
             .where('email' , '=' , req.body.email)
             .then(user => {
-                res.json('success login user')
+                res.json(user);
             })
             .catch(err => res.status(400).json('unable to register user'))
         }
@@ -92,7 +92,7 @@ app.post('/register',(req,res) => {
         .then(trx.commit)
         .catch(trx.rollback)
     })
-    .catch(err => res.status(400).json('unable to register user'))
+    .catch(err => res.status(400).json('email already exists'))
 })
 
 app.get('/profile/:id', (req,res) => {
